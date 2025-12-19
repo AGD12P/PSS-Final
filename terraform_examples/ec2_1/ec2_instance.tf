d511862e3c1d926f3c280d36561bd06e47a85158
AGD12P <alfonsogrageradorao@hotmail.com>
2025-12-19 10:05:36 +0100

    Update ec2_instance.tf

--- terraform_examples/ec2_1/ec2_instance.tf
+++ terraform_examples/ec2_1/ec2_instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2_resource" {
   instance_type   = "t2.micro"  
   key_name        = "key-pair"  # You should have created an EC2 key pair for SSH access
 
-  security_groups = [aws_Empresa_security_group.allow_ssh.name]
+  security_groups = [Empresa_security_group.allow_ssh.name]
 
   tags = {
     Name = "EC2Instance_Empresa"
