2b30a899f0c9517215c07832f809415d694ec8a1
AGD12P <alfonsogrageradorao@hotmail.com>
2025-12-19 10:02:00 +0100

    Create ec2_instance.tf

---
+++ terraform_examples/ec2_2/ec2_instance.tf
@@ -0,0 +1,11 @@
+# Define an EC2 instance resource
+resource "aws_instance" "my_ubuntu_ec2" {
+  ami             = "ami-12345678"  # Ubuntu 20.04 LTS AMI
+  instance_type   = "t3.micro"  
+  key_name        = "key-pair"  
+
+  security_groups = [aws_Empresa_security_group.allow_ssh_http.name]
+
+  tags = {
+    Name = "UbuntuEC2InstanceEmpresa"
+  }
